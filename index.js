43ad308c6d4a83cd66b8cdd9729d4ec3017e1ae7
Jim McGee <jmcgee86@gmail.com>
2017-11-28 13:34:02 +0000

    submit displays top headlines from chosen source

--- index.js
+++ index.js
@@ -19,6 +19,29 @@ $(document).ready(function(){
     })
      $("#source").submit(function(event){
         event.preventDefault();
-        alert(document.getElementById("selection").value);
-    });
+        //alert(document.getElementById("selection").value);
+        $.ajax({
+        method: "GET",
+        url: "https://newsapi.org/v2/top-headlines?sources=" + document.getElementById("selection").value,
+        data: { category: "business", country: "us", language: "en", apiKey: APIKEY},
+        success: function(data){
+            if (data.status ==="ok"){
+                console.log(data)
+              //document.getElementById("headlines").innerHTML = "testing";
+              for (var i=0; i < data.articles.length; i++){
+                  var item = document.createElement("LI");
+                  item.innerHTML = data.articles[i].title;
+                  	document.getElementById("headlines").appendChild(item);
+
+                  
+
+              }
+                    
+                }
+        }
+        
+    })
+        
+    })
+    
 })
